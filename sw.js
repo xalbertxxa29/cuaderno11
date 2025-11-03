a979d11a7c0b995f89344ae5b5bc86fb51e9f9d1
Juan Alberto Solis <jsolis@liderman.com>
2025-11-03 00:42:09 -0500

    sa
    asd

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-/* sw.js (v62)
+/* sw.js (v63)
    Estrategias:
    - HTML (navigate): network-first con timeout + navigationPreload + fallback a caché (ignoreSearch).
    - JS/CSS/Workers: stale-while-revalidate.
@@ -8,7 +8,7 @@
    - skipWaiting + clients.claim + autorefresh suave.
 */
 
-const SW_VERSION = 'v62';
+const SW_VERSION = 'v63';
 const PRECACHE   = `precache-${SW_VERSION}`;
 const RUNTIME    = `runtime-${SW_VERSION}`;
 
